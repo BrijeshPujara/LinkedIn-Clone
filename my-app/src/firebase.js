11bebf4629e2e5bb5514a72c143405ef655ab6f9
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-03-03 23:35:08 +0000

    Firebase connected, issues experienced with version and imports

---
+++ my-app/src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDSV7rP5pBlW-IZ8R7Sw3pVlkSpCcZnaho",
+    authDomain: "linkedin-clone-f8f25.firebaseapp.com",
+    projectId: "linkedin-clone-f8f25",
+    storageBucket: "linkedin-clone-f8f25.appspot.com",
+    messagingSenderId: "276248239202",
+    appId: "1:276248239202:web:0a19ad3a3acfa0a3aec53b",
+    measurementId: "G-ELPXGX85ZL"
+};
+  
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+
+export { db, auth };
+export default db;
